0f181c53e30d632c4e61f30c0afcdc11c69c53ed
Joel Ibaceta <niohnex@gmail.com>
2016-05-18 14:32:49 -0300

    random

--- random.rb
+++ random.rb
@@ -1,3 +1,9 @@
     load 'lib/mp-sdk-ruby/lib/mercadopago.rb'
     
 puts "Hello world";puts "ola k ase";puts "hello world 2";
+
+
+preference = MercadoPago::Preference.new
+preference.collector_id="202809963"
+preference.operation_type="regular_payment"
+preference.auto_return="approved"
