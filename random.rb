883154a8bcefe6200a270f635d607702e6a6a2e6
Joel Ibaceta <niohnex@gmail.com>
2016-05-19 11:24:51 -0300

    sdk

--- random.rb
+++ random.rb
@@ -16,62 +16,6 @@
     
     
    
-    preference = MercadoPago::Preference.new
+    item = MercadoPago::Item.new
 
-
-    preference.collector_id = "202809963"
-
-    preference.operation_type = "regular_payment"
-
-    preference.auto_return = "approved"
-    
-    puts preference.inspect
-
-
-    item1 = MercadoPago::Item.new
-
-
-    item1.id = "item-ID-1234"
-
-    item1.title = "Practical Granite Shirt"
-
-    item1.description = "Faker::Lorem.sentence Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies est sed elit interdum, quis consectetur libero mollis. Phasellus posuere sed eros vel volutpat."
-
-    item1.picture_url = "http://placehold.it/350x150"
-
-    item1.currency_id = "ARS"
-
-    item1.quantity = 2
-
-    item1.unit_price = 14.5
-
-
-    preference.items = [item1]
-
-    payer = MercadoPago::Payer.new
-
-
-    payer.name = ""
-
-    payer.surname = ""
-
-    payer.email = ""
-
-    payer.date_created = Time.now
-
-    payer.phone = { "area_code": '323', "number": '234223'}
-
-    payer.identification = { "type": "DNI", "number": "12345678" }
-
-    payer.address = { "street_name": "", "street_number": 4342, "zip_code": "U92393" }
-
-
-    preference.payer = payer
-
-    #puts preference.inspect
-
-     puts MercadoPago::Preference.friendly_print_structure
-
-    #puts preference.save
-    #
-    #puts preference.id
+    item.unit_price = 4
